aec1000ea4b4be9cc55b632796ce950cca792fce
Filippo Colpani <145765590+f-uni@users.noreply.github.com>
2024-05-04 14:46:35 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,24 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Creato il: Mag 03, 2024 alle 14:52
--- Versione del server: 8.0.32
--- Versione PHP: 8.0.22
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+SET time_zone = "+02:00";
 
 --
--- Database: `my_fcolpani`
+-- Database: `quiz`
 --
 
 -- --------------------------------------------------------
@@ -157,7 +142,3 @@ ALTER TABLE `partecipazione`
 ALTER TABLE `quiz`
   MODIFY `codice` int NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
