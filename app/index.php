46d58e6d1c84d3e2d95e9c1865c1ffde3d4346f3
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-04 18:27:06 +0200

    WIP gui

--- app/index.php
+++ app/index.php
@@ -2,14 +2,16 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="/app/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 
 <body>
-    <header>
-        titolo
-    </header>
+    <?php include "components/header.html" ?>
+    
     <main>
             <div class="side-menu">
                 <?php
