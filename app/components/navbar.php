264ba0b4a9bb1d889941e87746f0de7c50253c75
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-07 17:47:07 +0200

    pagine

--- app/components/navbar.php
+++ app/components/navbar.php
@@ -1,25 +1,25 @@
 <link rel="stylesheet" href="/app/css/navbar.css">
 <nav>
-    <a href="#">
-        <div class="nav-item active">
+    <a href="/app/index.php">
+        <div class="nav-item <?php if($page=="home") echo "active";?>">
             <i class="material-icons">home</i>
             Home
         </div>
     </a>
-    <a href="#">
-        <div class="nav-item">
+    <a href="/app/utenti.php">
+        <div class="nav-item <?php if($page=="utenti") echo "active";?>">
             <i class="material-icons">people</i>
             Utenti
         </div>
     </a>
-    <a href="#">
-        <div class="nav-item">
+    <a href="/app/quiz.php">
+        <div class="nav-item <?php if($page=="quiz") echo "active";?>">
             <i class="material-icons">question_answer</i>
             Quiz
         </div>
     </a>
-    <a href="#">
-        <div class="nav-item">
+    <a href="/app/partecipazioni.php">
+        <div class="nav-item <?php if($page=="partecipazioni") echo "active";?>">
             <i class="material-icons">comment</i>
             Partecipazioni
         </div>
