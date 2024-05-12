01365d715a8ba54f4cad311ade538c36bcf13f7f
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-12 19:09:33 +0200

    WIP tabella

--- app/quiz.php
+++ app/quiz.php
@@ -9,54 +9,55 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <?php
-    include "util/connect.php";
-    $page="quiz";
+include "util/connect.php";
+$page = "quiz";
 
-    $sql = 'SELECT * FROM quiz';
-    $stmt = $conn->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-    $stmt->execute([]);
-    $result = $stmt->fetchAll();
-    
+$sql = 'SELECT * FROM quiz';
+$stmt = $conn->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+$stmt->execute([]);
+$result = $stmt->fetchAll();
+
 ?>
+
 <body>
     <?php include "components/header.php" ?>
-    
+
     <main>
-            <div class="side-menu">
-                <?php
-                include "components/navbar.php"
-                ?>
-                <hr>
-                <div id="filter">
-                    <form action="#">
-                        <label for="testo">Testo</label>
-                        <input type="text" placeholder="Testo" name="testo">
-                        
-                        <label for="data">Data</label>
-                        <input type="date" placeholder="Data Inizio" name="data">
+        <div class="side-menu">
+            <?php
+            include "components/navbar.php"
+            ?>
+            <hr>
+            <div id="filter">
+                <form action="#">
+                    <label for="testo">Testo</label>
+                    <input type="text" placeholder="Testo" name="testo">
 
-                        <label for="numero">Numero</label>
-                        <input type="number" placeholder="Numero" name="numero">
-                        <br>
-                        <input type="submit" value="Invia">
+                    <label for="data">Data</label>
+                    <input type="date" placeholder="Data Inizio" name="data">
 
-                    </form>
-                </div>
-            
+                    <label for="numero">Numero</label>
+                    <input type="number" placeholder="Numero" name="numero">
+                    <br>
+                    <input type="submit" value="Invia">
+
+                </form>
+            </div>
+
         </div>
         <div class="content">
-
-            <table>
-                <thead>
-                    <th>Codice</th>
-                    <th>Creatore</th>
-                    <th>Titolo</th>
-                    <th>Data Inizio</th>
-                    <th>Data Fine</th>
-                </thead>
-                <tbody>
-                    <?php
-                        foreach($result as $row){
+            <div class="table-wrapper">
+                <table>
+                    <thead>
+                        <th>Codice</th>
+                        <th>Creatore</th>
+                        <th>Titolo</th>
+                        <th>Data Inizio</th>
+                        <th>Data Fine</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($result as $row) {
                             echo "<tr class='selectable'>
                                     <td>{$row['codice']}</td>
                                     <td>{$row['creatore']}</td>
@@ -65,10 +66,11 @@
                                     <td>{$row['data_fine']}</td>
                                 </tr>";
                         }
-                    ?>
-                </tbody>
+                        ?>
+                    </tbody>
 
-            </table>
+                </table>
+            </div>
         </div>
     </main>
     <?php
