dbf2aee911346ed4804d4e0755fac4a3995f0896
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-12 17:50:24 +0200

    Update quiz.php

--- app/quiz.php
+++ app/quiz.php
@@ -11,7 +11,12 @@
 <?php
     include "util/connect.php";
     $page="quiz";
-    
+
+    $sql = 'SELECT * FROM quiz';
+    $stmt = $conn->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+    $stmt->execute([]);
+    $result = $stmt->fetchAll();
+    print_r($result);
     
 ?>
 <body>
