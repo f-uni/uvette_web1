26c5583a15fe99ecdfb259bb3fbbf58d2a69b693
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-13 17:43:27 +0200

    Update quiz.php

--- app/quiz.php
+++ app/quiz.php
@@ -14,8 +14,25 @@ include "util/connect.php";
 $page = "quiz";
 
 $sql = 'SELECT * FROM quiz';
+$params=[];
+if (array_key_exists('nome_utente', $_GET)) {
+    $sql .= ' AND nome_utente like :nome_utente ';
+    $params["nome_utente"] = "%".$_GET["nome_utente"]."%";
+}
+if (array_key_exists('nome', $_GET)) {
+    $sql .= ' AND nome like :nome ';
+    $params["nome"] = "%".$_GET["nome"]."%";
+}
+if (array_key_exists('cognome', $_GET)) {
+    $sql .= ' AND cognome like :cognome ';
+    $params["cognome"] = "%".$_GET["cognome"]."%";
+}
+if (array_key_exists('email', $_GET)) {
+    $sql .= ' AND email like :email ';
+    $params["email"] = "%".$_GET["email"]."%";
+}
 $stmt = $conn->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
-$stmt->execute([]);
+$stmt->execute($params);
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
@@ -30,12 +47,19 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             ?>
             <hr>
             <div id="filter">
-                <form action="#">
-                    <label for="testo">Testo</label>
-                    <input type="text" placeholder="Testo" name="testo">
+                <form action=".">
 
-                    <label for="data">Data</label>
-                    <input type="date" placeholder="Data Inizio" name="data">
+                    <label for="titolo">Titolo</label>
+                    <input type="text" placeholder="Titolo" name="titolo">
+
+                    <label for="creatore">Creatore</label>
+                    <input type="text" placeholder="Creatore" name="creatore">
+
+                    <label for="data_inizio">Data Inizio</label>
+                    <input type="date"name="data_inizio">
+                    
+                    <label for="data_fine">Data Fine</label>
+                    <input type="date"name="data_fine">
 
                     <label for="numero">Numero</label>
                     <input type="number" placeholder="Numero" name="numero">
@@ -51,8 +75,8 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 <table>
                     <thead>
                         <th>Codice</th>
-                        <th>Creatore</th>
                         <th>Titolo</th>
+                        <th>Creatore</th>
                         <th>Data Inizio</th>
                         <th>Data Fine</th>
                     </thead>
@@ -62,8 +86,8 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                             $json=json_encode($row);
                             echo "<tr class='selectable'  onclick='displayQuiz(".$json.")'>
                                     <td>{$row['codice']}</td>
-                                    <td>{$row['creatore']}</td>
                                     <td>{$row['titolo']}</td>
+                                    <td>{$row['creatore']}</td>
                                     <td>{$row['data_inizio']}</td>
                                     <td>{$row['data_fine']}</td>
                                 </tr>";
