562d3cb40cc29c3ba897b057cf1a220b03787817
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-17 18:20:33 +0200

    WIP

--- app/partecipazioni.php
+++ app/partecipazioni.php
@@ -111,7 +111,7 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     </tbody>
                 </table>
                 
-                <button id="create-btn"><i class="material-icons">add</i></button>
+                <button id="create-btn"><i class="material-icons">add</i><p>Aggiungi</p></button>
             </div>
         </div>
     </main>
