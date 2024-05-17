b2489bf6b600176ec2d97314e9a29d5cbdc253bf
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-17 14:46:44 +0200

    Update quiz.php

--- app/quiz.php
+++ app/quiz.php
@@ -29,7 +29,10 @@ if (array_key_exists('creatore', $_GET)) {
 }
 if (array_key_exists('data_inizio', $_GET)) {
     if (array_key_exists('data_fine', $_GET)) {
-
+        //entrambe le date
+        $sql .= 'AND (data_inizio BETWEEN :data_inizio AND :data_fine) AND (data_fine BETWEEN :data_inizio AND :data_fine)';
+        $params["data_inizio"] = $_GET["data_inizio"];
+        $params["data_fine"] = $_GET["data_fine"];
     }else{
 
     }
