72732902fd384d56e5ce6b432f6930f21e9e4faa
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-19 22:54:37 +0200

    Update quiz.js

--- app/js/quiz.js
+++ app/js/quiz.js
@@ -3,5 +3,5 @@ function displayQuiz(event, value){
     if (event.target.tagName === 'a') {
         return;
     }
-    window.open("/app/mostraquiz.php?quiz="+value.codice, "_blank");
+    //window.open("/app/mostraquiz.php?quiz="+value.codice, "_blank");
 }
