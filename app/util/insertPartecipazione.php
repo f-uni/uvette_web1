ac869400a91d01220d6d3939f59f432b219eb755
Filippo Colpani <f.colpani3@studenti.unibg.it>
2024-05-22 09:18:11 +0200

    Update insertPartecipazione.php

--- app/util/insertPartecipazione.php
+++ app/util/insertPartecipazione.php
@@ -16,35 +16,48 @@ if (array_key_exists('utente', $_POST) && $_POST["utente"] != "" ) {
     $utente=$_POST["utente"];
     if(!exists_row("SELECT COUNT(*) FROM utente WHERE nome_utente=:utente", ["utente"=>$utente])){
         $err=true;
-        $message="Il nome utente inserito non esiste";
+        $message.="Il nome utente inserito non esiste\n";
     }
 }else{
     $err=true;
-    $message="Parametro utente non inserito";
+    $message.="Parametro utente non inserito\n";
 }
 
 if (array_key_exists('quiz', $_POST) && $_POST["quiz"] != "" ) {
     $quiz=$_POST["quiz"];
     if(!exists_row("SELECT COUNT(*) FROM quiz WHERE codice=:quiz", ["quiz"=>$quiz])){
         $err=true;
-        $message="Il quiz inserito non esiste";
+        $message.="Il quiz inserito non esiste\n";
     }
+
+    if (array_key_exists('data', $_POST) && $_POST["data"] != "" ) {
+        $data=$_POST["data"];
+        $stmt = $conn->prepare("SELECT COUNT(*) from quiz WHERE codice=:quiz AND :data BETWEEN data_inizio AND data_fine ", [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $stmt->execute(["quiz"=>$quiz, ":data"=>$data]);
+        if($stmt->fetchColumn()!=1){
+            $err=true;
+            $message.="La data inserita non è valida per il quiz selezionato\n";
+        }
+    }else{
+        $err=true;
+        $message.="Parametro data non inserito\n";
+    }
+
 }else{
     $err=true;
-    $message="Parametro quiz non inserito";
+    $message.="Parametro quiz non inserito\n";
 }
 
-if (array_key_exists('data', $_POST) && $_POST["data"] != "" ) {
-    $data=$_POST["data"];
-}else{
-    $err=true;
-    $message="Parametro data non inserito";
-}
+
 
 
 if($err){
     http_response_code(400);
     echo $message;
+}else{
+    http_response_code(200);
+    echo $utente." ".$quiz." ".$data;
+    
 }
 
 
